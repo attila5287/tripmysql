1d2bb000dd127b970dcb73827b1ff8b52b9041a2
attila5287 <atiturkoz@hotmail.com>
2021-03-25 08:58:00 -0600

    dev in main out

--- models/Trip.js
+++ models/Trip.js
@@ -1,51 +1,48 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-// create our Trip model
 class Trip extends Model {}
 
-// create fields/columns for Trip model
 Trip.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    trip_budget: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: true
-    },
-    traveller_amount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1
-    },
-    traveller_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'traveller',
-        key: 'id',
-        unique: false
-      }
-    },
-    location_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'location',
-        key: 'id',
-        unique: false
-      }
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'trip'
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		traveller_amount: {
+			type: DataTypes.INTEGER,
+			defaultValue: 1
+		},
+		trip_budget: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0
+		},
+		// Foreign Key
+		traveller_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "traveller",
+				key: "id"
+			}
+		},
+		// Foreign Key
+		location_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "location",
+				key: "id"
+			}
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "trip"
+	}
 );
 
 module.exports = Trip;

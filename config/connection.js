d7e40e8ab5ccb0871194b5a199ee8f33cedca017
attila5287 <atiturkoz@hotmail.com>
2021-03-26 10:44:59 -0600

    still app crashed

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const app_on_heroku = (process.env.ON_AIR == true);
+// const app_on_heroku = (process.env.ON_AIR == true);
 console.log('\n>> Connecting to DB at:');
 
 let sequelize; // conditionally branched
@@ -10,20 +10,21 @@ if (process.env.JAWSDB_URL) {
 	console.log("\t App runs Heroku:  JawsDB ");
 }
 
-if (!app_on_heroku) {
-console.log("\t App runs Local:  mySQL ");
-sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: "localhost",
-		dialect: "mysql",
-		port: 3306,
-	});
-console.log(" > password   : ", process.env.DB_PASSWORD);
-console.log(" > schema: " + process.env.DB_NAME + "(pre-set)");
-console.log(" > username   : ", process.env.DB_USER);	
+if (!process.env.JAWSDB_URL) {
+	console.log("\t App runs Local:  mySQL ");
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: "localhost",
+			dialect: "mysql",
+			port: 3306
+		}
+	);
+	console.log(" > password   : ", process.env.DB_PASSWORD);
+	console.log(" > schema: " + process.env.DB_NAME + "(pre-set)");
+	console.log(" > username   : ", process.env.DB_USER);
 }
 
 module.exports = sequelize;

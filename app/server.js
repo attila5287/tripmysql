f18871ddcbde20cf9855c1664c52535727bb2f3f
attila5287 <atiturkoz@hotmail.com>
2021-03-27 02:27:32 -0600

    del files add 1 by 1

--- app/server.js
+++ app/server.js
@@ -9,8 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-
-
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("\n>> App started and listens at:\n\t PORT :" + PORT + "\n")).catch((err) => {
 		console.log('err :>> ', err);
--- seed_sql/locs.sql
+++
@@ -1,9 +0,0 @@
--- jaws db name
-
-INSERT INTO z802j505xcs4tyz4.location
-    (id, location_name)
-  VALUES
-    (1, 'Istanbul'),
-    (2, 'Izmir'),
-    (3, 'Ankara')
-    ;

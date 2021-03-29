193923ac4f244db9a7f5278ebdd7ddc90f3a8d6c
attila5287 <atiturkoz@hotmail.com>
2021-03-29 01:04:01 -0600

    add index with forms

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,19 @@ const { Location, Traveller, Trip } = require("../models");
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
-    
+const path = require('path');
 const landing = require('./landing');
 // LANDING
 router.get("/", async (req, res) => {
+	try {
+		res.sendFile(path.join(__dirname, "index.html"));
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+
+router.get("/routes", async (req, res) => {
 	try {
 		res.status(200).json(landing);
 	} catch (err) {

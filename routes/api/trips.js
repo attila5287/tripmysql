959bfe21c004f29ce8294d9d4fedb2ce2802f751
attila5287 <atiturkoz@hotmail.com>
2021-03-29 02:41:28 -0600

    trip findAll with include err

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -1,5 +1,33 @@
 const router = require("express").Router();
-const { Trip } = require("../../models");
+const { Location, Traveller,Trip } = require("../../models");
+
+
+// GET all trips
+router.get("/", async (req, res) => {
+	try {
+		const tripData = await Trip.findAll();
+		res.status(200).json(tripData);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
+
+// GET a single location
+router.get("/:id", async (req, res) => {
+	try {
+		const trv = await Trip.findByPk(req.params.id);
+
+		if (!trv) {
+			res.status(404).json({ message: "No location found with this id!" });
+			return;
+		}
+
+		res.status(200).json(trv);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 
 // CREATE a trip
 router.post("/", async (req, res) => {
